a98f79464ae53aafccac72668860b78df57d7c0b
Michelle Marin <michelle@carinocreative.com>
2014-10-13 16:52:53 -0400

    okay finally stuff is actually showing up on my page now thank you justin

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,38 +1,38 @@
 "use strict";
 
-
-
-
-
-
-
 /* Functions */
 
 var myProfile = function () {
-  $.ajax({
-    url: 'https://api.github.com/users/michellemarin'
-  }).done(function (data) {
-
-    $('.info').text(JSON.stringify(data));
-
+  $.get('https://api.github.com/users/michellemarin', function (data) {
+    $('.info').html(JSON.stringify(data));
     setProfileInfo(data);
-
-  }).fail(function (data) {
-
-    $('.error').removeClass('fade').text(data.responseJSON.message);
-
-    setTimeout(function () {
-      $('.error').addClass('fade');
-    }, 1000);
-
   });
 };
 
+var myRepos = function () {
+$.getJSON('https://api.github.com/users/michellemarin/repos', function (data) {
+  setRepoData(data);
+});
+};
 
-var setProfileInfo = function (myProfile) {
-$('#profileContainer').html(
-_.template($('#profileTemplate').html())(myProfile));
-}
+
+var setProfileInfo = function (data) {
+  var myTemplate = _.template($('#profileTemplate').html());
+  $('#profileContainer').append(myTemplate(data));
+};
+
+
+var setRepoData = function (data) {
+  _.each(data, function (repo) {
+    var stupidTemplate = _.template($('#repoTemplate').html());
+    $('#repoContainer').append(stupidTemplate(repo));
+    console.log(stupidTemplate);
+    console.log(repo);
+  });
+};
+
+myProfile();
+myRepos();
 
 
 /* API calls */
