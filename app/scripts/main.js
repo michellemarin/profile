49ae830cd594a398a7c0d052ae782dff9c1ffe26
Michelle Marin <michelle@carinocreative.com>
2014-10-15 18:14:29 -0400

    made css changes, button classes, etc

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -35,4 +35,16 @@ myProfile();
 myRepos();
 
 
-/* API calls */
+/* Accordian */
+
+$(document).ready(function(){
+  $('ul.tabs li').click(function(){
+    var tab_id = $(this).attr('data-tab');
+
+    $('ul.tabs li').removeClass('current');
+    $('.tab-content').removeClass('current');
+
+    $(this).addClass('current');
+    $("#"+tab_id).addClass('current');
+  });
+});
